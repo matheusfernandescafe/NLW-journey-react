8a5149c31e4bcdf4f5d480fd297ddb43864f759e
Matheus Fernandes <matheusfernandesofc@gmail.com>
2024-07-12 22:38:52 -0300

    feat: Realizado integração com API

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -5,6 +5,8 @@ import { DestinationAndDateStep } from "./steps/destination-and-date-step";
 import { InviteGuestsStep } from "./steps/invite-guests-step";
 import { Rodape } from "./rodape";
 import { useNavigate } from "react-router-dom";
+import type { DateRange } from "react-day-picker";
+import { api } from "../../lib/axios";
 
 export function CreateTripPage() {
   const navigate = useNavigate();
@@ -13,6 +15,13 @@ export function CreateTripPage() {
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
   const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false);
   const [emailsToInvite, setEmailsToInvite] = useState(["teste@email.com"]);
+
+  const [destination, setDestination] = useState("");
+  const [ownerName, setOwnerName] = useState("");
+  const [ownerEmail, setOwnerEmail] = useState("");
+  const [eventStartAndEndDates, setEventStartAndEndDates] = useState<
+    DateRange | undefined
+  >();
 
   function changeGuestsInput() {
     setIsGuestsInputOpen(!isGuestsInputOpen);
@@ -42,15 +51,50 @@ export function CreateTripPage() {
   }
 
   function removeEmailFromInvites(emailToRemove: string) {
-    const newEmailList = emailsToInvite.filter((invited) => invited !== emailToRemove);
+    const newEmailList = emailsToInvite.filter(
+      (invited) => invited !== emailToRemove
+    );
 
     setEmailsToInvite(newEmailList);
   }
 
-  function createTrip(event: FormEvent<HTMLFormElement>) {
+  async function createTrip(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    navigate("/trips/123");
+    console.log(destination);
+    console.log(ownerName);
+    console.log(ownerEmail);
+    console.log(emailsToInvite);
+    console.log(eventStartAndEndDates);
+
+    if (!destination) {
+      return;
+    }
+
+    if (!eventStartAndEndDates?.from || !eventStartAndEndDates?.to) {
+      return;
+    }
+
+    if (emailsToInvite.length === 0) {
+      return;
+    }
+
+    if (!ownerName || !ownerEmail) {
+      return;
+    }
+
+    const response = await api.post("/trips", {
+      destination: destination,
+      starts_at: eventStartAndEndDates?.from,
+      ends_at: eventStartAndEndDates?.to,
+      emails_to_invite: emailsToInvite,
+      owner_name: ownerName,
+      owner_email: ownerEmail,
+    });
+
+    const { tripId } = response.data;
+
+    navigate(`/trips/${tripId}`);
   }
 
   return (
@@ -58,21 +102,49 @@ export function CreateTripPage() {
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
         <div className="flex flex-col items-center gap-3">
           <img src="/logo.svg" alt="plann.er" />
-          <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+          <p className="text-zinc-300 text-lg">
+            Convide seus amigos e planeje sua próxima viagem!
+          </p>
         </div>
 
         <div className="space-y-4">
-          <DestinationAndDateStep isGuestsInputOpen={isGuestsInputOpen} changeGuestsInput={changeGuestsInput} />
+          <DestinationAndDateStep
+            isGuestsInputOpen={isGuestsInputOpen}
+            changeGuestsInput={changeGuestsInput}
+            setDestination={setDestination}
+            setEventStartAndEndDates={setEventStartAndEndDates}
+            eventStartAndEndDates={eventStartAndEndDates}
+          />
 
-          {isGuestsInputOpen && <InviteGuestsStep changeModalInput={changeModalInput} emailsToInvite={emailsToInvite} changeConfirmaTripModalOpen={changeConfirmaTripModalOpen} />}
+          {isGuestsInputOpen && (
+            <InviteGuestsStep
+              changeModalInput={changeModalInput}
+              emailsToInvite={emailsToInvite}
+              changeConfirmaTripModalOpen={changeConfirmaTripModalOpen}
+            />
+          )}
         </div>
 
         <Rodape />
       </div>
 
-      {isGuestsModalOpen && <InviteGuestsModal emailsToInvite={emailsToInvite} addNewEmailToInvite={addNewEmailToInvite} changeModalInput={changeModalInput} removeEmailFromInvites={removeEmailFromInvites} />}
+      {isGuestsModalOpen && (
+        <InviteGuestsModal
+          emailsToInvite={emailsToInvite}
+          addNewEmailToInvite={addNewEmailToInvite}
+          changeModalInput={changeModalInput}
+          removeEmailFromInvites={removeEmailFromInvites}
+        />
+      )}
 
-      {isConfirmTripModalOpen && <ConfirmTripModal changeConfirmaTripModalOpen={changeConfirmaTripModalOpen} createTrip={createTrip} />}
+      {isConfirmTripModalOpen && (
+        <ConfirmTripModal
+          changeConfirmaTripModalOpen={changeConfirmaTripModalOpen}
+          createTrip={createTrip}
+          setOwnerName={setOwnerName}
+          setOwnerEmail={setOwnerEmail}
+        />
+      )}
     </div>
   );
 }
--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,16 +1,41 @@
-import { MapPin, Calendar, Settings2, ArrowRight } from "lucide-react";
-import React from "react";
+import { MapPin, Calendar, Settings2, ArrowRight, X } from "lucide-react";
+import React, { useState } from "react";
 import { Button } from "../../../components/button";
+import { DayPicker, type DateRange } from "react-day-picker";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import "react-day-picker/dist/style.css";
 
 interface DestinationAndDateStepProps {
   isGuestsInputOpen: boolean;
   changeGuestsInput: () => void;
+  setDestination: (destination: string) => void;
+  setEventStartAndEndDates: (dates: DateRange | undefined) => void;
+  eventStartAndEndDates: DateRange | undefined;
 }
 
 export function DestinationAndDateStep({
   isGuestsInputOpen,
   changeGuestsInput,
+  setDestination,
+  setEventStartAndEndDates,
+  eventStartAndEndDates,
 }: DestinationAndDateStepProps) {
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+
+  function changeDatePickerOpen() {
+    setIsDatePickerOpen(!isDatePickerOpen);
+  }
+
+  const displayedDate =
+    eventStartAndEndDates &&
+    eventStartAndEndDates.from &&
+    eventStartAndEndDates.to
+      ? format(eventStartAndEndDates.from, "d' de 'LLL")
+          .concat(" até ")
+          .concat(format(eventStartAndEndDates.to, "d' de 'LLL"))
+      : "Quando?";
+
   return (
     <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
       <div className="flex items-center gap-2 flex-1">
@@ -20,18 +45,44 @@ export function DestinationAndDateStep({
           type="text"
           placeholder="Para onde você vai?"
           className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+          onChange={(event) => setDestination(event.target.value)}
         />
       </div>
 
-      <div className="flex items-center gap-2">
+      <button
+        disabled={isGuestsInputOpen}
+        onClick={changeDatePickerOpen}
+        className="flex items-center gap-2 text-left w-[240px]"
+      >
         <Calendar className="size-5 text-zinc-400" />
-        <input
-          disabled={isGuestsInputOpen}
-          type="text"
-          placeholder="Quando??"
-          className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
-        />
-      </div>
+        <span className="txt-lg text-zinc-400 w-40 flex-1 ">
+          {displayedDate}
+        </span>
+      </button>
+
+      {isDatePickerOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Selecione a data</h2>
+                <button type="button" onClick={changeDatePickerOpen}>
+                  <X className="size-5 text-zinc-400" />
+                </button>
+              </div>
+            </div>
+            <DayPicker
+              mode="range"
+              locale={ptBR}
+              selected={eventStartAndEndDates}
+              onSelect={setEventStartAndEndDates}
+              disabled={{
+                before: new Date(),
+              }}
+            />
+          </div>
+        </div>
+      )}
 
       <div className="w-px h-6 bg-zinc-800"></div>
 
